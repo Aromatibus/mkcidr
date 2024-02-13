5cbb5206a9c99459f712b6895d131afb43d73ac0
Aromatibus <aromablackcoffee@yahoo.co.jp>
2024-02-13 18:02:19 +0900

    RIR same checker

--- src/rir-samechk.py
+++ src/rir-samechk.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 
 # Extract data for the same IP address from the RIR list
 
@@ -11,16 +10,10 @@
 import os
 import sys
 import time
-from concurrent.futures import ThreadPoolExecutor, as_completed
-from datetime import datetime
 from logging import INFO, Formatter, StreamHandler, getLogger
 from urllib.parse import urlparse
 
-import requests
 import tqdm
-from requests.adapters import HTTPAdapter
-from requests.exceptions import ConnectionError, HTTPError, RequestException, Timeout
-from urllib3.util import Retry
 
 
 def init_logger() -> None:
@@ -31,76 +24,6 @@ def init_logger() -> None:
     logger = getLogger()
     logger.addHandler(handler)
     logger.setLevel(INFO)
-    return
-
-
-def allow_downloads(allow_time_min: int, URLs: list) -> bool:
-    # Determine whether to continue based on the time(min)
-    # the file was downloaded
-    current_time = datetime.now().timestamp()
-    for url in URLs:
-        file_name = os.path.basename(urlparse(url).path)
-        if not os.path.exists(file_name):
-            return True
-        download_time = os.path.getmtime(file_name)
-        difference_time = (current_time - download_time) / 60
-        if difference_time > allow_time_min:
-            return True
-    return False
-
-
-def download(url) -> bool:
-    rir_filename = os.path.basename(urlparse(url).path)
-    rir_registry = rir_filename.split("-")[1]
-    getLogger().info("download start : %s", rir_registry)
-    try:
-        retry = Retry(
-            total=5,  # retry n times
-            backoff_factor=2,  # wait 1, 2, 4, 8, 16 sec
-            status_forcelist=[429, 500, 502, 503, 504],
-        )  # retry when status code is ...
-        session = requests.Session()
-        session.mount("http://", HTTPAdapter(max_retries=retry))
-        response = requests.get(url, timeout=(15.0, 15.0))
-        session.close()
-        response.raise_for_status()
-    except ConnectionError as e:
-        getLogger().error("  connection error : %s", rir_registry + " (" + str(e) + ")")
-        return False
-    except HTTPError as e:
-        getLogger().error("  http error : %s", rir_registry + " (" + str(e) + ")")
-        return False
-    except Timeout as e:
-        getLogger().error("  timeout error : %s", rir_registry + " (" + str(e) + ")")
-        return False
-    except RequestException as e:
-        getLogger().error("  download error : %s", rir_registry + " (" + str(e) + ")")
-        return False
-    else:
-        if response.status_code == 200:
-            with open(rir_filename, "wb") as file:
-                file.write(response.content)
-        else:
-            getLogger().error("  download error : %s", rir_registry)
-            return False
-    getLogger().info("download end   : %s", rir_registry)
-    return True
-
-
-def parallel_download(URLs: list) -> bool:
-    getLogger().info("download task start")
-    with ThreadPoolExecutor(max_workers=5) as executor:
-        futures = [executor.submit(download, url) for url in URLs]
-        for future in as_completed(futures):
-            if not future.result():
-                return False
-    for url in URLs:
-        filename = os.path.basename(urlparse(url).path)
-        if not os.path.exists(filename):
-            getLogger().error("  download error : %s", filename + " (file not found)")
-            return False
-    getLogger().info("download task end")
-    return True
 
 
 def change_rir_format(line: str) -> str:
@@ -129,14 +52,14 @@ def restore_rir_format(line: str) -> str:
 
 def rir_load_reformat_ipv4(RIR_URLs: list, EXCLUDED_COUNTRIES: list) -> list:
     getLogger().info("RIR Same Checker ipv4 : start")
-    check_list = list()
+    check_list = []
     check_list_append = check_list.append
     getLogger().info("RIR Same Checker ipv4 : Load RIR")
     for rir_url in RIR_URLs:
         rir_filename = os.path.basename(urlparse(rir_url).path)
         rir_registry = rir_filename.split("-")[1]
         rir_path = os.path.abspath(os.path.join(os.getcwd(), rir_filename))
-        with open(rir_path, "r") as file:
+        with open(rir_path) as file:
             for line in file:
                 if line.startswith("#"):
                     continue
@@ -159,14 +82,14 @@ def rir_same_checker_ipv4(check_list: list) -> None:
     path_ipv4 = os.path.abspath(os.path.join(os.getcwd(), "ipv4"))
     if not os.path.exists(path_ipv4):
         os.makedirs(path_ipv4)
-    same_list = list()
+    same_list = []
     same_list_append = same_list.append
     if str(check_list[0].split("|")[0]) == str(check_list[1].split("|")[0]):
         required_param = restore_rir_format(check_list[0])
         same_list.append(required_param)
     for line in tqdm.tqdm(check_list):
         if str(line.split("|")[0]) == str(
-            check_list[check_list.index(line) - 1].split("|")[0]
+            check_list[check_list.index(line) - 1].split("|")[0],
         ):
             required_param = restore_rir_format(line)
             same_list_append(required_param)
@@ -174,19 +97,22 @@ def rir_same_checker_ipv4(check_list: list) -> None:
         if line == check_list[-1]:
             continue
         if str(line.split("|")[0]) == str(
-            check_list[check_list.index(line) + 1].split("|")[0]
+            check_list[check_list.index(line) + 1].split("|")[0],
         ):
             required_param = restore_rir_format(line)
             same_list_append(required_param)
             continue
     getLogger().info("RIR Same Checker ipv4 : Write File")
     with open(
-        path_ipv4 + "/_Same_RIR.ipv4", "w", encoding="utf-8", newline="\n"
+        path_ipv4 + "/_Same_RIR.ipv4",
+        "w",
+        encoding="utf-8",
+        newline="\n",
     ) as outfile:
         for line in same_list:
             outfile.write(line)
+    getLogger().info("Number of the same address : %s", int(len(same_list) / 2))
     getLogger().info("RIR Same Checker ipv4 : end")
-    return
 
 
 if __name__ == "__main__":
@@ -224,18 +150,7 @@ if __name__ == "__main__":
     os.chdir(DIR_IP_LISTS)
 
     start = time.time()
-    allow_time_min = 18 * 60  # 18 hours
-    if allow_downloads(allow_time_min, RIR_URLs):
-        if not parallel_download(RIR_URLs):
-            print("The download was canceled because an error occurred.")
-            sys.exit(1)
-        print("download time : {:,.2f} sec".format(time.time() - start))
-    else:
-        print("The download was canceled because the specified time has not elapsed.")
-
-    print("")
-    start = time.time()
     rir_same_checker_ipv4(rir_load_reformat_ipv4(RIR_URLs, EXCLUDED_COUNTRIES))
-    print("processing time : {:,.2f} sec".format(time.time() - start))
+    print(f"processing time : {time.time() - start:,.2f} sec")
 
     sys.exit(0)
