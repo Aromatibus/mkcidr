473981b99d12704bbe08eda4f109b32d890d404d
Aromatibus <aromablackcoffee@yahoo.co.jp>
2024-02-20 18:49:20 +0900

    テストデータの重複を確認するコードを追加しました

--- tests/dup.py
+++ tests/dup.py
@@ -1,15 +1,17 @@
 # https://note.nkmk.me/python-pandas-duplicated-drop-duplicates/#subset
 
-import os
 from pathlib import Path
 
 import pandas as pd
 
-os.chdir(Path.cwd())
 Path_CSV = Path("./testdata").resolve()
 
 data_frame = pd.read_csv(Path_CSV)
 
+print("Original:")
 print(data_frame)
+print()
 
+print("Duplicated:")
 print(data_frame.duplicated())
+print()
