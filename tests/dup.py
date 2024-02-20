da7f8c256871779b3ee6ebeeb86651d54db3516b
Aromatibus <aromablackcoffee@yahoo.co.jp>
2024-02-20 18:43:13 +0900

    コードの変更点を説明するために、次のコミットメッセージを使用してください。***
    ***テストデータとdup.pyに変更を加えました。***

--- tests/dup.py
+++ tests/dup.py
@@ -1,11 +1,15 @@
 # https://note.nkmk.me/python-pandas-duplicated-drop-duplicates/#subset
 
+import os
 from pathlib import Path
 
 import pandas as pd
 
+os.chdir(Path.cwd())
 Path_CSV = Path("./testdata").resolve()
 
 data_frame = pd.read_csv(Path_CSV)
 
 print(data_frame)
+
+print(data_frame.duplicated())
