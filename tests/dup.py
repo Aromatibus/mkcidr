4b333ca7b725ad7ecf7318936eea3724a607ee3d
Aromatibus <aromablackcoffee@yahoo.co.jp>
2024-02-20 18:30:00 +0900

    パスの指定方法を変更しました***

--- tests/dup.py
+++ tests/dup.py
@@ -1,7 +1,11 @@
 # https://note.nkmk.me/python-pandas-duplicated-drop-duplicates/#subset
 
+from pathlib import Path
+
 import pandas as pd
 
-data_frame = pd.read_csv("testdata")
+Path_CSV = Path("./testdata").resolve()
+
+data_frame = pd.read_csv(Path_CSV)
 
 print(data_frame)
